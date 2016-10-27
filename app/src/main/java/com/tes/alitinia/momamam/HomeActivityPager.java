07a5e7475081db036a57ae9af75150fc48c9b11f
ALITINIA <alitiniahasan@yahoo.com>
2016-10-27 20:34:42 +0700

    first commit

---
+++ app/src/main/java/com/tes/alitinia/momamam/HomeActivityPager.java
@@ -0,0 +1,13 @@
+package com.tes.alitinia.momamam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivityPager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_pager);
+    }
+}
